7dd17179435ca5c994a2a06a522b1e001b4cd0dd
Student User <student@workstation.lab.example.com>
2023-12-29 05:30:10 -0500

    Fix bug

--- todo-ssr/temp/post-commit.py
+++ todo-ssr/temp/post-commit.py
@@ -1,9 +1,6 @@
 import time
 
 def liveness_check():
-    while True:
-    # Add any custom liveness checks here
-    # For simplicity, just print a message
     print('Liveness check: Application is alive')
     time.sleep(5)  # Adjust the sleep time based on your requirements
 
